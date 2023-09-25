80ed762986cdf3c94a68355be691d8a0cb20d5b7
zurcx <luizfabiodacruz@gmail.com>
2023-09-25 08:44:46 -0300

    construcao admin.py

--- backend/servico/admin.py
+++ backend/servico/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from .models import Servico, OrdemServico, OrdemServicoItem
 
+class OrdemServicoItemInline(admin.TabularInline):
+    model = OrdemServicoItem
+    extra = 0
+
+@admin.register(OrdemServico)
+class OrdemServicoAdmin(admin.ModelAdmin):
+    inlines = (OrdemServicoItemInline,)
+    list_display = ('__str__', 'cliente', 'situacao')
+    list_display_links = ('cliente',)
+    search_fields = ('cliente__razao_social',)
+    list_filter = ('situacao',)
+    
+    
+
 admin.site.register(Servico)
-admin.site.register(OrdemServico)
-admin.site.register(OrdemServicoItem)
